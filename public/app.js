0687491bf82acb7d360b9eeafd0a38f1644643cd
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-01-04 15:54:54 -0800

    Updated controller and index, angular now working as it should. However, unable to make get request as the url is attempting to go through local host

--- public/app.js
+++ public/app.js
@@ -1,23 +1,37 @@
 angular.module('WhatToWear', [])
-  // .factory('Weather', function($http) {
-  //   var getWeather = function($http, zipcode) {
-  // 	  return $http({
-  // 	    method: "GET",
-  // 	  	url:'http:api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&APPID=5c680e5d8c8f29befb9f1c239dfae90b'
-  // 	  }); 
-  // 	};
-  // 	return {
-  // 	  getWeather: getWeather
-  // 	}; 
-  // })
+  .factory('Weather', function($http) {
+    var getWeather = function(zipcode) {
+  	  return $http({
+  	    method: 'GET',
+  	  	url:'http:api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&APPID=5c680e5d8c8f29befb9f1c239dfae90b'
+  	  }).success(function (data) {
+        console.log(data.name); 
+      })
+  	};
+  	return {
+  	  getWeather: getWeather
+  	}; 
+  })
 
-  .controller('weatherController', function($scope) {
+  .controller('weatherController', function($scope, Weather) {
     $scope.destinations = []; 
 
-    $scope.zip = 'testing';
+    $scope.zip = '94611';
 
     $scope.addDest = function(destination) {
       $scope.destinations.push(destination); 
       $scope.zip = ''; 
-    }
+    };
+
+    $scope.functionTest = function() {
+      console.log('it worked');
+    };
+
+    $scope.getInfo = function() {
+     console.log('making request');
+     Weather.getWeather($scope.zip); 
+    };
   })
+
+
+
