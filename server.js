9050957d896f5011f404d842621991364e3b3a62
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-01-04 12:00:41 -0800

    Updated index.html to run Angular and have working input bar that adds zip code to an unordered list

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ var port = 3000;
 var ip = '127.0.0.1';
 
 var server = http.createServer(function(req, res) {
+  console.log("Serving " + req.method + "request for " + req.url); 
   if(req.method === 'GET') {
   	//be able to serve all static files. 
+  	if(req.url === '/') {
+  	  fs.readFile('index.html', 'utf8', function(err, data) {
+  	  	res.writeHead(200, {'Content-type': 'text/html'});
+  	  	res.end(data); 
+  	  })
+  	} else {
+  	  fs.readFile(req.url, 'utf8', function(err, data) {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        if(data) {
+          res.write(data)
+        }
+        res.end();  
+  	  })
+    }
   }
-})
+});
+
+server.listen(port, ip); 
