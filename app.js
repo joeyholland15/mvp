0a5b8c5e620105d7a4e0e1ef67c8d226f20b6a74
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-01-04 15:09:16 -0800

    Implemented test to make sure index view is connected to app controller - it is

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('WhatToWear', [])
+  .factory('Weather', function($http) {
+    var getWeather = function($http, zipcode) {
+  	  return $http({
+  	    method: "GET",
+  	  	url:'http:api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&APPID=5c680e5d8c8f29befb9f1c239dfae90b'
+  	  }); 
+  	};
+  	return {
+  	  getWeather: getWeather
+  	}; 
+  })
+
+  .controller('weatherController', function($scope, Weather) {
+    $scope.destinations = []; 
+
+    $scope.zip = '';
+
+    $scope.addDest = function(destination) {
+      $scope.destinations.push(destination); 
+      $scope.zip = ''; 
+    }
+  })
--- public/app.js
+++ public/app.js
@@ -1,20 +1,20 @@
 angular.module('WhatToWear', [])
-  .factory('Weather', function($http) {
-    var getWeather = function($http, zipcode) {
-  	  return $http({
-  	    method: "GET",
-  	  	url:'http:api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&APPID=5c680e5d8c8f29befb9f1c239dfae90b'
-  	  }); 
-  	};
-  	return {
-  	  getWeather: getWeather
-  	}; 
-  })
+  // .factory('Weather', function($http) {
+  //   var getWeather = function($http, zipcode) {
+  // 	  return $http({
+  // 	    method: "GET",
+  // 	  	url:'http:api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&APPID=5c680e5d8c8f29befb9f1c239dfae90b'
+  // 	  }); 
+  // 	};
+  // 	return {
+  // 	  getWeather: getWeather
+  // 	}; 
+  // })
 
-  .controller('weatherController', function($scope, Weather) {
+  .controller('weatherController', function($scope) {
     $scope.destinations = []; 
 
-    $scope.zip = '';
+    $scope.zip = 'testing';
 
     $scope.addDest = function(destination) {
       $scope.destinations.push(destination); 
