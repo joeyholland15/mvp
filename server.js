ef8a98cc80b0d2810c64e35d7f670934af424ee0
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-01-04 13:17:05 -0800

    Moved index/css files into public directory and refactored server to use express and styles now load

--- server.js
+++ server.js
@@ -32,9 +32,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+app.use(express.static('public'));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
